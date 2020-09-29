af2bfbceb846aafb60fec09ac4753dc9e01d1768
Michael Brich <mike@toreda.com>
2020-09-28 20:21:41 -0700

    Changed double quotes to single per sonar rules.

--- tests/transforms.spec.ts
+++ tests/transforms.spec.ts
@@ -1,76 +1,76 @@
-import {TBTransform} from "../src/transform";
-import {TBTransforms} from "../src/transforms";
+import {TBTransform} from '../src/transform';
+import {TBTransforms} from '../src/transforms';
 
-const MOCK_STRING = "oneoneone";
+const MOCK_STRING = 'oneoneone';
 
-describe("TBTransforms", () => {
+describe('TBTransforms', () => {
 	let instance: TBTransforms<string>;
 
 	beforeAll(() => {
 		instance = new TBTransforms<string>(MOCK_STRING);
 	});
 
-	describe("Constructor", () => {
-		it("shall pass", () => {});
+	describe('Constructor', () => {
+		it('shall pass', () => {});
 	});
 
-	describe("Implementation", () => {
-		describe("add", () => {
+	describe('Implementation', () => {
+		describe('add', () => {
 			let custom: TBTransforms<string>;
-			const MOCK_FALLBACK1 = "22222233333___11";
+			const MOCK_FALLBACK1 = '22222233333___11';
 
 			beforeEach(() => {
 				custom = new TBTransforms<string>(MOCK_FALLBACK1);
 			});
 
-			it("should return false when transform argument is null", () => {
+			it('should return false when transform argument is null', () => {
 				expect(custom.add(null as any)).toBe(false);
 			});
 
-			it("should return false when transform argument is undefined", () => {
+			it('should return false when transform argument is undefined', () => {
 				expect(custom.add(undefined as any)).toBe(false);
 			});
 		});
 
-		describe("addNB", () => {
+		describe('addNB', () => {
 			let custom: TBTransforms<string>;
-			const MOCK_FALLBACK1 = "22222233333___11";
+			const MOCK_FALLBACK1 = '22222233333___11';
 
 			beforeEach(() => {
 				custom = new TBTransforms<string>(MOCK_FALLBACK1);
 			});
 
-			it("should return false when transform argument is null", () => {
+			it('should return false when transform argument is null', () => {
 				expect(custom.addNB(null as any)).toBe(false);
 			});
 
-			it("should return false when transform argument is undefined", () => {
+			it('should return false when transform argument is undefined', () => {
 				expect(custom.addNB(undefined as any)).toBe(false);
 			});
 		});
 
-		describe("run", () => {
+		describe('run', () => {
 			let custom: TBTransforms<string>;
-			const MOCK_FALLBACK1 = "22222233333___11";
+			const MOCK_FALLBACK1 = '22222233333___11';
 			beforeEach(() => {
 				custom = new TBTransforms<string>(MOCK_FALLBACK1);
 			});
 
-			it("should return fallback default when value argument is null", () => {
+			it('should return fallback default when value argument is null', () => {
 				expect(custom.run(null as any)).toBe(MOCK_FALLBACK1);
 			});
 
-			it("should return fallback default when value argument is undefined", () => {
+			it('should return fallback default when value argument is undefined', () => {
 				expect(custom.run(undefined as any)).toBe(MOCK_FALLBACK1);
 			});
 
-			it("should return value argument when no transforms are applied", () => {
-				const inputValue = "one_one_two";
+			it('should return value argument when no transforms are applied', () => {
+				const inputValue = 'one_one_two';
 				expect(custom.transforms).toHaveLength(0);
 				expect(custom.run(inputValue)).toBe(inputValue);
 			});
 
-			it("should execute all transforms", () => {
+			it('should execute all transforms', () => {
 				const tf1 = jest.fn();
 				tf1.mockImplementation((val: string) => {
 					return val;
@@ -96,7 +96,7 @@ describe("TBTransforms", () => {
 				expect(tf2).not.toHaveBeenCalled();
 				expect(tf3).not.toHaveBeenCalled();
 
-				const val = "aaaaaaaaaa4414";
+				const val = 'aaaaaaaaaa4414';
 				const result = custom.run(val);
 
 				expect(tf1).toHaveBeenCalledTimes(1);
@@ -105,23 +105,23 @@ describe("TBTransforms", () => {
 			});
 		});
 
-		describe("runNullable", () => {
+		describe('runNullable', () => {
 			let custom: TBTransforms<string>;
-			const MOCK_FALLBACK1 = "22222233333___11";
+			const MOCK_FALLBACK1 = '22222233333___11';
 			beforeEach(() => {
 				custom = new TBTransforms<string>(MOCK_FALLBACK1);
 			});
 
-			it("should return null when value argument is null", () => {
+			it('should return null when value argument is null', () => {
 				expect(custom.runNB(null as any)).toBeNull();
 			});
 
-			it("should return null when value argument is undefined", () => {
+			it('should return null when value argument is undefined', () => {
 				expect(custom.runNB(undefined as any)).toBeNull();
 			});
 
-			it("should return value argument when no transforms are applied", () => {
-				const inputValue = "one_one_two";
+			it('should return value argument when no transforms are applied', () => {
+				const inputValue = 'one_one_two';
 				expect(custom.transforms).toHaveLength(0);
 				expect(custom.runNB(inputValue)).toBe(inputValue);
 			});
