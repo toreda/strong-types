75eec5c85d65420ee3614e0367b8091c8a656b9f
Michael Brich <mike@toreda.com>
2020-10-07 12:24:52 -0700

    Added per property validation tests for DOM Styles Font + Text.

--- tests/map/dom-styles/font.spec.ts
+++ tests/map/dom-styles/font.spec.ts
@@ -1,14 +1,32 @@
 import {DOMStylesFont} from '../../../src/map/dom-styles/font';
 
-const EMPTY_OBJECT = {};
-const EMPTY_STRING = '';
-
-describe('DOMStylesFont', () => {
+const EXPECTED_STRONG_PROPERTIES = [
+	{name: 'enabled', expectedValueType: 'boolean', expectedValue: true},
+	{name: 'color', expectedValueType: 'string', expectedValue: '#FFFFFF'},
+	{name: 'size', expectedValueType: 'string', expectedValue: '12px'},
+	{name: 'family', expectedValueType: 'string', expectedValue: 'sans-serif'},
+	{name: 'weight', expectedValueType: 'string', expectedValue: 'normal'}
+];
+describe('DomStylesFont', () => {
 	let instance: DOMStylesFont;
 
 	beforeAll(() => {
 		instance = new DOMStylesFont();
 	});
 
-	describe('Constructor', () => {});
+	describe('Constructor', () => {
+		for (const prop of EXPECTED_STRONG_PROPERTIES) {
+			describe(`strong property ${prop.name}`, () => {
+				it(`should be a function`, () => {
+					expect(instance).toHaveProperty(prop.name);
+					expect(typeof instance[prop.name]).toBe('function');
+				});
+
+				it(`should have initial value '${prop.expectedValue}'`, () => {
+					expect(typeof instance[prop.name]()).toBe(prop.expectedValueType);
+					expect(instance[prop.name]()).toBe(prop.expectedValue);
+				});
+			});
+		}
+	});
 });
--- tests/map/dom-styles/text.spec.ts
+++ tests/map/dom-styles/text.spec.ts
@@ -1,5 +1,11 @@
 import {DOMStylesText} from '../../../src/map/dom-styles/text';
 
+const EXPECTED_STRONG_PROPERTIES = [
+	{name: 'enabled', expectedValueType: 'boolean', expectedValue: true},
+	{name: 'decoration', expectedValueType: 'string', expectedValue: 'none'},
+	{name: 'shadow', expectedValueType: 'string', expectedValue: '0'}
+];
+
 describe('DOMStylesText', () => {
 	let instance: DOMStylesText;
 
@@ -8,13 +14,18 @@ describe('DOMStylesText', () => {
 	});
 
 	describe('Constructor', () => {
-		it('should define property enabled as StrongType', () => {
-			expect(instance).toHaveProperty('enabled');
-			expect(typeof instance.enabled).toBe('function');
-		});
+		for (const prop of EXPECTED_STRONG_PROPERTIES) {
+			describe(`strong property ${prop.name}`, () => {
+				it(`should be a function`, () => {
+					expect(instance).toHaveProperty(prop.name);
+					expect(typeof instance[prop.name]).toBe('function');
+				});
 
-		it('should initialize enabled to true', () => {
-			expect(instance.enabled()).toBe(true);
-		});
+				it(`should have initial value '${prop.expectedValue}'`, () => {
+					expect(typeof instance[prop.name]()).toBe(prop.expectedValueType);
+					expect(instance[prop.name]()).toBe(prop.expectedValue);
+				});
+			});
+		}
 	});
 });
