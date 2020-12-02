f92aea03f807807e88e8fb1dba7364d18f738fad
catwhitmer <catwhitmer@gmail.com>
2020-12-02 10:23:52 -0500

    created mock values and describe method

---
+++ tests/types/string.spec.ts
@@ -0,0 +1,9 @@
+import { StrongString, makeString } from '../../src/types/string';
+
+const MOCK_INITIAL = 'hello';
+const MOCK_FALLBACK_DEFAULT = 'hello world';
+const MOCK_FALLBACK = 'world';
+
+describe('StrongString', () => {
+    describe('Implementation', () => {});
+});
