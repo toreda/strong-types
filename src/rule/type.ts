f2bf39b392da1eea97e25e88bf4d927fd911dce6
catwhitmer <catwhitmer@gmail.com>
2020-12-15 14:58:02 -0500

    updated the types

--- src/rule/type.ts
+++ src/rule/type.ts
@@ -1,11 +1,9 @@
 import {STOpIsArray, makeIsArray} from '../validator/is-array';
 import {STOpIsBoolean, makeIsBoolean} from '../validator/is-boolean';
-import {STOpIsDate, makeIsDate} from '../validator/is-date';
 import {STOpIsDouble, makeIsDouble} from '../validator/is-double';
 import {STOpIsInteger, makeIsInteger} from '../validator/is-integer';
 import {STOpIsNull, makeIsNull} from '../validator/is-null';
 import {STOpIsString, makeIsString} from '../validator/is-string';
-import {STOpIsTime, makeIsTime} from '../validator/is-time';
 
 import {STRule} from '../rule/rule';
 import {STRuleModifiers} from '../rule/modifiers';
@@ -17,8 +15,6 @@ export class STRuleType {
 	public readonly array: STOpIsArray<STRuleType>;
 	public readonly string: STOpIsString<STRuleType>;
 	public readonly null: STOpIsNull<STRuleType>;
-	public readonly date: STOpIsDate<STRuleType>;
-	public readonly time: STOpIsTime<STRuleType>;
 
 	constructor(rule: STRule, mods: STRuleModifiers) {
 		this.array = makeIsArray<STRuleType>(this, rule, mods);
@@ -27,7 +23,5 @@ export class STRuleType {
 		this.integer = makeIsInteger<STRuleType>(this, rule, mods);
 		this.string = makeIsString<STRuleType>(this, rule, mods);
 		this.null = makeIsNull<STRuleType>(this, rule, mods);
-		this.date = makeIsDate<STRuleType>(this, rule, mods);
-		this.time = makeIsTime<STRuleType>(this, rule, mods);
 	}
 }
