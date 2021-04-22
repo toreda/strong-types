afef8e76930759a4e9b2046a739379ee94343140
Cat Whitmer <catwhitmer@gmail.com>
2021-04-22 10:27:00 -0400

    added no argument test

--- tests/types/numbers.spec.ts
+++ tests/types/numbers.spec.ts
@@ -6,17 +6,21 @@ interface TestType {
 	name: string;
 	instance: any;
 }
-const double: StrongDouble = makeDouble(0);
-const int: StrongInt = makeInt(0);
-const uint: StrongUInt = makeUInt(0);
-
-const TEST_TYPES: TestType[] = [
-	{name: 'double', instance: double},
-	{name: 'int', instance: int},
-	{name: 'uint', instance: uint}
-];
+let double: StrongDouble;
+let int: StrongInt;
+let uint: StrongUInt;
+let testTypes: TestType[];
 
 describe('numberMethods', () => {
+	double = makeDouble(0);
+	int = makeInt(0);
+	uint = makeUInt(0);
+	testTypes = [
+		{name: 'double', instance: double},
+		{name: 'int', instance: int},
+		{name: 'uint', instance: uint}
+	];
+
 	beforeEach(() => {
 		double.reset();
 		int.reset();
@@ -24,10 +28,21 @@ describe('numberMethods', () => {
 	});
 
 	describe('Usage', () => {
-		for (const testType of TEST_TYPES) {
+		for (const testType of testTypes) {
 			describe(`Type: ${testType.name}`, () => {
 				describe('increment', () => {
-					it(`should`, () => {});
+					it(`should return initial value when called with no arguments`, () => {
+						const result = testType.instance();
+						expect(result).toBe(0);
+					});
+
+				});
+
+				describe('decrement', () => {
+					it(`should return initial value when called with no arguments`, () => {
+						const result = testType.instance();
+						expect(result).toBe(0);
+					});
 				});
 			});
 		}
