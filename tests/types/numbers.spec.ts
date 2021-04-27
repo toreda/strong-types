d6a4db9288afdfea58197bbfc6043b9edd246150
Cat Whitmer <catwhitmer@gmail.com>
2021-04-27 12:54:45 -0400

    added increment/decrement tests

--- tests/types/numbers.spec.ts
+++ tests/types/numbers.spec.ts
@@ -2,10 +2,13 @@ import {StrongDouble, makeDouble} from '../../src/types/double';
 import {StrongInt, makeInt} from '../../src/types/int';
 import {StrongUInt, makeUInt} from '../../src/types/uint';
 
+import {StrongNumber} from '../../dist/strong-number';
+
 interface TestType {
 	name: string;
 	instance: any;
 }
+
 let double: StrongDouble;
 let int: StrongInt;
 let uint: StrongUInt;
@@ -31,31 +34,38 @@ describe('numberMethods', () => {
 		for (const testType of testTypes) {
 			describe(`Type: ${testType.name}`, () => {
 				describe('increment', () => {
-					it(`should return initial value when called with no arguments`, () => {
-						const result = testType.instance();
-						expect(result).toBe(0);
+					it(`should increment by 1 when the value is a number`, () => {
+						const value = 5;
+						const result = testType.instance;
+						result(value);
+						expect(result()).toBe(value);
+						result.increment();
+						expect(result()).toBe(6);
 					});
 
-					it(`should return null when called with null value`, () => {
-						const result = testType.instance(null);
-						expect(result).toBeNull;
-					});
-
-					it(`should return undefined`, () => {
-						const result = testType.instance().increment;
-						expect(result).toBeUndefined;
-					});
-
-					it(`should call increment method`, () => {
-						testType.instance().increment;
-						expect(testType.instance).toHaveBeenCalled;
+					it(`should return fallback when the value is null`, () => {
+						const value = null;
+						const result = testType.instance;
+						result(value);
+						expect(result()).toBe(0);
 					});
 				});
 
 				describe('decrement', () => {
-					it(`should return initial value when called with no arguments`, () => {
-						const result = testType.instance();
-						expect(result).toBe(0);
+					it(`should decrement by 1 when the value is a number`, () => {
+						const value = 5;
+						const result = testType.instance;
+						result(value);
+						expect(result()).toBe(value);
+						result.decrement();
+						expect(result()).toBe(4);
+					});
+
+					it(`should return fallback when the value is null`, () => {
+						const value = null;
+						const result = testType.instance;
+						result(value);
+						expect(result()).toBe(0);
 					});
 				});
 			});
