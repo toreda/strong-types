3004d010127ec5e47012dab01bb588095bb32d06
Michael Brich <mike@toreda.com>
2021-05-13 13:22:29 -0700

    Removed @armorjs/build and replaced with the updated version @toreda/build-tools. Upgraded project package dev dependencies

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,30 +1,32 @@
-import { dest, parallel, series, src } from 'gulp';
+import {series, src} from 'gulp';
 
-import { ArmorBuild } from '@armorjs/build';
-import { EventEmitter } from 'events';
+import {BuildTools} from '@toreda/build-tools';
+import {EventEmitter} from 'events';
 const eslint = require('gulp-eslint');
 
-const build: ArmorBuild = new ArmorBuild(new EventEmitter());
+const build: BuildTools = new BuildTools(new EventEmitter());
 
 function runLint() {
-    return src(['src/**'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
+	return (
+		src(['src/**'])
+			// eslint() attaches the lint output to the "eslint" property
+			// of the file object so it can be used by other modules.
+			.pipe(eslint())
+			// eslint.format() outputs the lint results to the console.
+			// Alternatively use eslint.formatEach() (see Docs).
+			.pipe(eslint.format())
+			// To have the process exit with an error code (1) on
+			// lint error, return the stream and pipe to failAfterError last.
+			.pipe(eslint.failAfterError())
+	);
 }
 
 function createDist() {
-	return build.create.dir('./dist', false);
+	return build.steps.createDir('./dist');
 }
 
 function cleanDist() {
-	return build.clean.dir('./dist');
+	return build.steps.cleaner.dir('./dist');
 }
 
 function buildSrc() {
